0d6daa03d0aae42d9754d33ed904637d046ccbfe
fwkz <fwkz@ymail.com>
2012-11-09 16:31:52 +0100

    Butterworth low-pass

--- main.py
+++ main.py
@@ -10,40 +10,40 @@ class Filter(object):
         self.fs = fs
         self.gpass =  gpass
         self.gstop = gstop
+        self.sample_freq = 1000.0
 
         #Computing omegas [rad/s]
         self.wp = 2 * numpy.pi * self.fp
         self.ws = 2 * numpy.pi * self.fs
 
-        #Computing sampling frequency due to Whittaker-Nyquist-Kotelnikov-Shannon law.
+        ##Computing sampling frequency due to Whittaker-Nyquist-Kotelnikov-Shannon law.
         if self.wp > self.ws:
             self.sampling_w = 2 * self.wp
-            #self.sampling_w = 2 * numpy.pi * self.sampling_freq 
         else:
             self.sampling_w = 2 * self.ws
-            #self.sampling_w = 2 * numpy.pi * self.sampling_freq 
         
         #Normalizing omegas due to Nyquist frequency.
         self.wp_norm = self.wp / (self.sampling_w / 2)
         self.ws_norm = self.ws / (self.sampling_w / 2)
   
     def butterworth(self):
-        (ord, wn) = signal.buttord(self.wp_norm, self.ws_norm, self.gpass, self.gstop, analog=0)
+        (ord, wn) = signal.buttord(self.wp_norm, self.ws_norm, self.gpass, self.gstop, analog=True)
         print ord
         print wn
 
-        (b, a) = signal.butter(ord, wn, btype='low', analog=1, output='ba')
+        (b, a) = signal.butter(ord, wn, btype="lowpass", analog=True, output='ba')
+        (w, h) = signal.freqs(b, a, worN=1000)
 
         # filter frequency response
-        (w, h) = signal.freqs(b, a)
-        pyplot.plot(w, numpy.abs(h))
+        pyplot.semilogx(w, numpy.abs(h))
         pyplot.title('Filter Frequency Response')
         pyplot.text(5e-2, 1e-3, str(ord) + "-th order Butterworth filter")
         pyplot.grid(True)
+        pyplot.vlines(wn, 0, 1.2, color='k', linestyles='dashdot', label="wn")
         pyplot.show()
     
     def chebyshev(self):
         """To be implemented"""
         pass
 
-filter = Filter(500, 1000, 3, 20).butterworth()
+filter = Filter(2000, 1000, 1.0, 20.0).butterworth()
